9eb7e2af34d2269128d47b6afb1dd03df2079558
mx <max@maxvl.com>
2023-08-11 17:31:52 -0400

    call the subdomain code and print the output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() -> Result<(), anyhow::Error> {
+use reqwest::blocking::Client;
+use std::error::Error;
+
+mod subdomains;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let http_client = Client::new();
+    let target = "example.com";
+
+    let subdomains = subdomains::enumerate(&http_client, target);
+
+    println!("{:#?}", subdomains);
+
     Ok(())
 }
