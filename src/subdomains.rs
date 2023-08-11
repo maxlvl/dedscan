3b768ebec1467e3191ddbbf9b3b3f238180e12b9
mx <max@maxvl.com>
2023-08-10 22:24:12 -0400

    define subdomains and create TODO

---
+++ src/subdomains.rs
@@ -0,0 +1,9 @@
+// TODO:
+
+// create public function called enumerate
+// call crt.sh's api endpoint and specify output to be json
+// declare a CrtEntryResult struct and specify what it should be depending on the response
+// filter and deduplicate the responses and pour that into a hashSet (TBD)
+
+// when that is done - declare a SubDomain struct that contains the subdomain info and open_ports, then pour your existing
+// subdomains into a Vec of that struct with a non-initialized open_ports (as you'll add that in the port scanner
