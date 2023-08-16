5bdcd94f94e13a88e38f1ff1c1d552ca8ed68d5e
mx <max@maxvl.com>
2023-08-15 22:48:04 -0400

    Finish up port scanning logic and adding some print statements

--- src/ports.rs
+++ src/ports.rs
@@ -3,10 +3,12 @@ use crate::{
     model::{Port, SubDomain},
 };
 
-use std::net::{SocketAddrs, TcpStream, ToSocketAddrs};
+use std::net::{SocketAddr, TcpStream, ToSocketAddrs};
+use std::time::Duration;
 
 pub fn scan_ports(mut subdomain: SubDomain) -> SubDomain {
-    let socket_addrs: Vec<SocketAddrs> = format("{}:1024", subdomain.domain)
+    println!("Scanning ports for subdomain");
+    let socket_addrs: Vec<SocketAddr> = format!("{}:1024", subdomain.domain)
         .to_socket_addrs()
         .expect("Port Scanner: Establishing TCP Connection via socket address")
         .collect();
@@ -17,5 +19,30 @@ pub fn scan_ports(mut subdomain: SubDomain) -> SubDomain {
             subdomain.domain
         );
         return subdomain;
+    }
+
+    subdomain.open_ports = MOST_COMMON_PORTS_100
+        .into_iter()
+        .map(|port| scan_port(socket_addrs[0], *port))
+        .filter(|port| port.is_open) // filter out closed ports
+        .collect();
+
+    subdomain
+}
+
+pub fn scan_port(mut socket_addr: SocketAddr, port: u16) -> Port {
+    println!("Checking socket_addr {} on port {}", socket_addr, port);
+    let timeout = Duration::from_secs(3);
+    socket_addr.set_port(port);
+
+    let is_open = if let Ok(_) = TcpStream::connect_timeout(&socket_addr, timeout) {
+        true
+    } else {
+        false
+    };
+
+    Port {
+        port: port,
+        is_open: is_open,
     }
 }
